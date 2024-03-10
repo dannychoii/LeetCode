c68519285d5d098ccd77d1e0eed02484ce4f51d2
dwchoi <justicejustice314@gmail.com>
2024-03-11 01:21:17 +0900

    3.Longest-Substring-Without-Repeating-Characters.kt

--- kotlin/Solved/Sliding-Window/3.Longest-Substring-Without-Repeating-Characters.kt
+++ kotlin/Solved/Sliding-Window/3.Longest-Substring-Without-Repeating-Characters.kt
@@ -1,25 +1,17 @@
-class Main() {
-    fun main() {
-        println(Solution().lengthOfLongestSubstring("abcabcbb"))
-    }
-}
-
 class Solution {
     fun lengthOfLongestSubstring(s: String): Int {
-
         val set = mutableSetOf<Char>()
-
         var start = 0
         var end = 0
         var ans = 0
-        while(start<=end){
-            if(!set.contains(s[end])){
+        while (start <= end && end <= s.lastIndex) {
+            if (!set.contains(s[end])) {
                 set.add(s[end])
                 end++
-            } else{
                 ans = max(ans, set.size)
-                set.delete(s[start])
-                start ++
+            } else {
+                set.remove(s[start])
+                start++
             }
         }
         return ans
